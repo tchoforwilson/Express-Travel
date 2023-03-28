e7ba399639750ded0adaba8969d6ee588ff9020a
tchoforwilson <tchoforwilson@gmail.com>
2023-03-28 17:27:25 +0100

    Bus routes implementation

---
+++ routes/busRoutes.js
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import busController from '../controllers/busController';
+import authController from '../controllers/authController';
+
+const router = Router({ mergeParams: true });
+
+router
+  .route('/')
+  .get(busController.getAllBuses)
+  .post(
+    authController.protect,
+    authController.restrictTo('agency'),
+    busController.createBus
+  );
+
+router
+  .route('/:id')
+  .get(busController.getBus)
+  .patch(
+    authController.protect,
+    authController.restrictTo('agency'),
+    busController.updateBus
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('agency'),
+    busController.deletBus
+  );
+
+export default Router;
