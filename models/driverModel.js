bb46a3ca043af4836193859106c20dcd71658905
tchoforwilson <tchoforwilson@gmail.com>
2023-03-28 16:52:31 +0100

    Definition of driver model collection

---
+++ models/driverModel.js
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+import validator from 'validator';
+
+const driverSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide driver name'],
+  },
+  contact: {
+    type: Number,
+    required: [true, 'Please provide driver contact'],
+    validate: [validator.isMobilePhone, 'Please provide a valid contact'],
+  },
+  photo: String,
+  agency: {
+    type: Schema.ObjectId,
+    ref: 'Agency',
+    required: [true, 'Driver must belong to an agency'],
+  },
+});
+
+const Driver = model('Driver', driverSchema);
+
+export default Driver;
