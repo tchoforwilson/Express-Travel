78340fe1920ca432b039a3fc97789bb86e08848f
tchoforwilson <tchoforwilson@gmail.com>
2023-04-03 20:45:29 +0100

    travel controller for travels

--- controllers/travelController.js
+++ controllers/travelController.js
@@ -1,17 +1,26 @@
 import factory from './handlerFactory.js';
 import Travel from '../models/travelModel.js';
 
+const setTravelAgencyId = (req, res, next) => {
+  if (!req.body.agency)
+    req.body.agency = req.user.agency.id || req.params.agencyId;
+  next();
+};
+
 const createTravel = factory.createOne(Travel);
-const getTravel = factory.getOne(Travel);
+const getTravel = factory.getOne(Travel, {
+  path: 'driver',
+  select: 'name contact photo',
+});
 const getAllTravels = factory.getAll(Travel);
 const updateTravel = factory.updateOne(Travel);
 const deletTravel = factory.deleteOne(Travel);
 
 export default {
-    createTravel,
-    getTravel,
-    getAllTravels,
-    updateTravel,
-    deletTravel
-}
-
+  setTravelAgencyId,
+  createTravel,
+  getTravel,
+  getAllTravels,
+  updateTravel,
+  deletTravel,
+};
