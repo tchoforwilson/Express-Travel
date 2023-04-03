e80a0995d4ea1eca3477489bbcc810c246c9eb11
tchoforwilson <tchoforwilson@gmail.com>
2023-04-03 11:04:05 +0100

    refactored user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,56 +1,67 @@
 import { Schema, model } from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Please provide agency name'],
-  },
-  contact: {
-    type: Number,
-    unique: [true, 'Contact already exist'],
-    validate: [validator.isMobilePhone, 'Please provide a valid contact'],
-  },
-  role: {
-    type: String,
-    enum: ['admin', 'agency'],
-    default: 'agency',
-  },
-  address: {
-    city: String,
-    address_line: String,
-  },
-  photo: String,
-  password: {
-    type: String,
-    required: [true, 'Please provide a password!'],
-    minLength: 8,
-    select: false,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Please confirm your password'],
-    validate: {
-      // This only works on CREATE and SAVE!!!
-      validator: function (el) {
-        return el === this.password;
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please tell us your name!'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      unique: [true, 'User already exist'],
+      lowercase: true,
+      validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'manager', 'user'],
+      default: 'user',
+    },
+    photo: {
+      type: String,
+      default: 'default.jpg',
+    },
+    agency: {
+      type: Schema.ObjectId,
+      ref: 'Agency',
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+      minlength: 8,
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password'],
+      validate: {
+        // This only works on CREATE and SAVE!!!
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: 'Passwords are not the same!',
       },
-      message: 'Passwords are not the same!',
+    },
+    passwordChangedAt: Date,
+    active: {
+      type: Boolean,
+      default: true,
+      select: false,
     },
   },
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  active: {
-    type: Boolean,
-    default: true,
-    select: false,
-  },
-});
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    },
+  }
+);
 
-/**
- * @breif middleware to check for password hash
- */
+userSchema.index({ role: 'admin' }, { unique: true });
+
 userSchema.pre('save', async function (next) {
   // Only run this function if password was actually modified
   if (!this.isModified('password')) return next();
@@ -63,9 +74,6 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-/**
- * @breif middleware to check for password change
- */
 userSchema.pre('save', function (next) {
   if (!this.isModified('password') || this.isNew) return next();
 
@@ -79,12 +87,6 @@ userSchema.pre(/^find/, function (next) {
   next();
 });
 
-/**
- * @breif Model method to check for correct password
- * @details This is done by comparing the submitted password with the saved password in the collection
- * @param {String} candidatePassword -> User submitted password
- * @param {String} userPassword -> User password in the collection
- */
 userSchema.methods.correctPassword = async function (
   candidatePassword,
   userPassword
@@ -92,11 +94,6 @@ userSchema.methods.correctPassword = async function (
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-/**
- * @breif middleware to check for password change time
- * @details check if the password recently change after the user login
- * @param {Date} -> JWTTimestamp -> The JWT time is token
- */
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
     const changedTimestamp = parseInt(
