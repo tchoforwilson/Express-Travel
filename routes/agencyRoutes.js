d49c09f87e0f1a249dd8cc2b731daa0a9f8aca3d
tchoforwilson <tchoforwilson@gmail.com>
2023-04-03 20:43:51 +0100

    added use params for various agency actors

--- routes/agencyRoutes.js
+++ routes/agencyRoutes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 import authController from '../controllers/authController.js';
 import agencyController from '../controllers/agencyController.js';
+import userRouter from './userRoutes.js';
+import driverRouter from './driverRoutes.js';
+import busRouter from './busRoutes.js';
+import travelRouter from './travelRoutes.js';
 
 const router = Router();
 
+// Merge parameters
+router.use('/:agencyId/users', userRouter);
+router.use('/:agencyId/drivers', driverRouter);
+router.use('/:agencyId/buses', busRouter);
+router.use('/:agencyId/travels', travelRouter);
+
+// Unprotected route
 router.get('/', agencyController.getAllAgencies);
 router.get('/:id', agencyController.getAgency);
 
