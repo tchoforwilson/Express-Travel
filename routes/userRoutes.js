f7377c5517c5bceec6b273f1b47bddd8df1912ec
tchoforwilson <tchoforwilson@gmail.com>
2023-04-03 11:03:42 +0100

    refactored user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,42 @@
+import { Router } from 'express';
+import userController from './../controllers/userController.js';
+import authController from './../controllers/authController.js';
+
+const router = Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.post(
+  '/register',
+  authController.restrictTo('admin', 'manager'),
+  authController.register
+);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch(
+  '/updateMe',
+  userController.uploadUserPhoto,
+  userController.resizeUserPhoto,
+  userController.updateMe
+);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin', 'manager'));
+
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+export default router;
