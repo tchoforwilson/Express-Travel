6282365fdec0dc0c6dafbbc20860ea0559b112b5
tchoforwilson <tchoforwilson@gmail.com>
2023-04-03 11:04:30 +0100

    refactored user controller

---
+++ controllers/userController.js
@@ -0,0 +1,93 @@
+import sharp from 'sharp';
+import User from './../models/userModel.js';
+import catchAsync from './../utilities/catchAsync.js';
+import AppError from './../utilities/appError.js';
+import factory from './handlerFactory.js';
+import upload from '../utilities/upload.js';
+import { filterObj } from '../utilities/util.js';
+
+const uploadUserPhoto = upload.single('photo');
+
+const resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/images/users/${req.file.filename}`);
+
+  next();
+});
+
+const getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
+const updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+const deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined! Please use /signup instead',
+  });
+};
+
+const getUser = factory.getOne(User);
+const getAllUsers = factory.getAll(User);
+
+// Do NOT update passwords with this!
+const updateUser = factory.updateOne(User);
+const deleteUser = factory.deleteOne(User);
+
+export default {
+  uploadUserPhoto,
+  resizeUserPhoto,
+  getMe,
+  updateMe,
+  deleteMe,
+  createUser,
+  getUser,
+  getAllUsers,
+  updateUser,
+  deleteUser,
+};
